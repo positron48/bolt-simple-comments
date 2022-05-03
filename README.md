2c1f484e7e7defecedf4fc6eb86ee86905c62ee5
Filatov Anton <positron48@gmail.com>
2022-05-04 00:06:19 +0300

    add comment entity

--- README.md
+++ README.md
@@ -9,18 +9,3 @@ Installation:
 ```bash
 composer require positron48/comment-extension
 ```
-
-
-## Running PHPStan and Easy Codings Standard
-
-First, make sure dependencies are installed:
-
-```
-COMPOSER_MEMORY_LIMIT=-1 composer update
-```
-
-And then run ECS:
-
-```
-vendor/bin/ecs check src
-```
---
+++ src/Entity/Comment.php
@@ -0,0 +1,136 @@
+<?php
+
+
+namespace Positron48\CommentExtension\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="comment")
+ */
+class Comment
+{
+    /**
+     * @var int
+     * @Assert\Unique()
+     * @Assert\NotBlank()
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @ORM\Column(type="string")
+     */
+    protected $authorName;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @ORM\Column(type="string")
+     */
+    protected $authorEmail;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(min = 5)
+     * @ORM\Column(type="string")
+     */
+    protected $message;
+
+    /**
+     * @var int
+     * @Assert\NotBlank()
+     * @ORM\Column(type="integer")
+     */
+    protected $contentId;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorName(): string
+    {
+        return $this->authorName;
+    }
+
+    /**
+     * @param string $authorName
+     */
+    public function setAuthorName(string $authorName): void
+    {
+        $this->authorName = $authorName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorEmail(): string
+    {
+        return $this->authorEmail;
+    }
+
+    /**
+     * @param string $authorEmail
+     */
+    public function setAuthorEmail(string $authorEmail): void
+    {
+        $this->authorEmail = $authorEmail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getContentId(): int
+    {
+        return $this->contentId;
+    }
+
+    /**
+     * @param int $contentId
+     */
+    public function setContentId(int $contentId): void
+    {
+        $this->contentId = $contentId;
+    }
+
+
+}
